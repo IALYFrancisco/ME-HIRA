3590fd6b262cec10a7de626e1d4cfa5eb71b3b80
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-03-05 20:29:03 +0300

    choix du logo du plateforme

---
+++ src/components/LogoComponent.js
@@ -0,0 +1,9 @@
+import Styles from './LogoComponent.module.css'
+
+function LogoComponent(){
+    return(
+        <img src="/images/icons8-musical-100.png" alt="Logo de me-hira" className={Styles.logo}/>
+    )
+}
+
+export default LogoComponent
--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,18 +1,19 @@
 import ButtonComponent from '../components/ButtonComponent'
+import LogoComponent from '../components/LogoComponent';
 
 function HomeView () {
     return (
         <header>
             <nav>
-            <div class="logo-container">
-                <h1>Logo</h1>
-            </div>
-            <div class="searchbar-container">
-                <input type="text" name="" id="" placeholder="Faite une recherche ..."/>
-            </div>
-            <div class="button-container">
-                <ButtonComponent text="Se connecter"/>
-            </div>
+                <div class="logo-container">
+                    <LogoComponent/>
+               </div>
+                <div class="searchbar-container">
+                    <input type="text" name="" id="" placeholder="Faite une recherche ..."/>
+                </div>
+                <div class="button-container">
+                    <ButtonComponent text="Se connecter"/>
+                </div>
             </nav>
       </header>
     )
