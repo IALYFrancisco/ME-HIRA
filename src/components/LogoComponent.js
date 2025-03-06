ef08f490a5826029c15cd7756f1db37bdfe1f291
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-03-06 20:50:19 +0300

    suppression de fichiers inutiles

--- src/components/LogoComponent.js
+++ src/components/LogoComponent.js
@@ -2,7 +2,7 @@ import Styles from './LogoComponent.module.css'
 
 function LogoComponent(){
     return(
-        <img src="/images/icons8-musical-100.png" alt="Logo de me-hira" className={Styles.logo}/>
+        <img src="/images/icons8-music-record-100.png" alt="Logo de me-hira" className={Styles.logo}/>
     )
 }
 
