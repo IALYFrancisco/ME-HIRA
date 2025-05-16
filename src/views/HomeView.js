4efa38ec97d9a9cfdfc97aebb263263a584cc05e
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-05-16 12:45:20 +0300

    développement de la liste des chansons

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -3,19 +3,30 @@ import LogoComponent from '../components/LogoComponent';
 
 function HomeView () {
     return (
-        <header>
-            <nav>
-                <div class="logo-container">
-                    <LogoComponent/>
-               </div>
-                <div class="searchbar-container">
-                    <input type="text" name="" id="" placeholder="Faite une recherche ..."/>
+        <body>
+            <header>
+                <nav>
+                    <div class="logo-container">
+                        <LogoComponent/>
                 </div>
-                <div class="button-container">
-                    <ButtonComponent text="Se connecter"/>
-                </div>
-            </nav>
-        </header>
+                    <div class="searchbar-container">
+                        <input type="text" name="" id="" placeholder="Faite une recherche ..."/>
+                    </div>
+                    <div class="button-container">
+                        <ButtonComponent text="Se connecter"/>
+                    </div>
+                </nav>
+            </header>
+            <main>
+                <aside></aside>
+                <section>
+                    <div class="music">
+                        
+                    </div>
+                </section>
+                <aside></aside>
+            </main>
+        </body>
     )
 }
 
