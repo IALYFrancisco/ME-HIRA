f53960b26995de47cd220e06f7f6312eefefadb0
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-06-06 21:10:02 +0300

    amélioration des listes de chansons

--- src/components/Song/SongComponent.js
+++ src/components/Song/SongComponent.js
@@ -7,14 +7,16 @@ function SongComponent({src}){
             <img src='/images/logo-de-me-hira.png' alt='Logo de ME-HIRA' className={Styles.img_logo} />
             <section className={Styles.song_info}>
                 <section className={Styles.left}>
-                    <h4 className={Styles.title}>Sambatra</h4>
+                    <h4 className={Styles.title}><em>Sambatra</em></h4>
                     <h6 className={Styles.author}>Rolf</h6>
                 </section>
                 <section className={Styles.right}>
                     <div className={Styles.play_container}>
                         <img src='/images/play.png' className={Styles.play} alt='icon play de me-hira'/>
                     </div>
-                    <img src='/images/options.png' className={Styles.options} alt='icon opttions de me-hira'/>
+                    <div className={Styles.play_container}>
+                        <img src='/images/options.png' className={Styles.options} alt='icon opttions de me-hira'/>
+                    </div>
                 </section>
             </section>
         </li>
