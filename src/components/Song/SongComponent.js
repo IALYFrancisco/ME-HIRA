eefe6a90b7a9c6d5dc23123f0f873cde3cfa0380
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-06-20 13:33:10 +0300

    redéfinition du composant chanson

--- src/components/Song/SongComponent.js
+++ src/components/Song/SongComponent.js
@@ -1,19 +1,58 @@
-import { useRef } from 'react'
+// import { useRef } from 'react'
+// import Styles from './SongComponent.module.css'
+
+// function SongComponent({src, title, author}){
+
+//     const songRef = useRef(null)
+
+//     const playSong = () => {
+//         if(songRef.current){
+//             songRef.current.play()
+//         }
+//     }
+
+//     return(
+//         <li className={Styles.song}>
+//             <audio src={src} ref={songRef} ></audio>
+//             <img src='/images/logo-de-me-hira.png' alt='Logo de ME-HIRA' className={Styles.img_logo} />
+//             <section className={Styles.song_info}>
+//                 <section className={Styles.left}>
+//                     <h4 className={Styles.title}>{title}</h4>
+//                     <h6 className={Styles.author}>{author}</h6>
+//                 </section>
+//                 <section className={Styles.right}>
+//                     <div className={Styles.play_container} onClick={playSong}>
+//                         <img src='/images/play.png' className={Styles.play} alt='icon play de me-hira'/>
+//                     </div>
+//                     <div className={Styles.play_container}>
+//                         <img src='/images/options.png' className={Styles.options} alt='icon opttions de me-hira'/>
+//                     </div>
+//                 </section>
+//             </section>
+//         </li>
+//     )
+// }
+
+// export default SongComponent
+
+
+import { useRef, useContext } from 'react'
 import Styles from './SongComponent.module.css'
+import { SongContext } from '../../contexts/SongContext';
 
-function SongComponent({src, title, author}){
+function SongComponent({src, title, author}) {
+    const songRef = useRef(null);
+    const { play } = useContext(SongContext);
 
-    const songRef = useRef(null)
-
-    const playSong = () => {
-        if(songRef.current){
-            songRef.current.play()
+    const togglePlay = () => {
+        if (songRef.current) {
+            play(songRef.current);
         }
-    }
+    };
 
-    return(
+    return (
         <li className={Styles.song}>
-            <audio src={src} ref={songRef} ></audio>
+            <audio src={src} ref={songRef}></audio>
             <img src='/images/logo-de-me-hira.png' alt='Logo de ME-HIRA' className={Styles.img_logo} />
             <section className={Styles.song_info}>
                 <section className={Styles.left}>
@@ -21,16 +60,16 @@ function SongComponent({src, title, author}){
                     <h6 className={Styles.author}>{author}</h6>
                 </section>
                 <section className={Styles.right}>
-                    <div className={Styles.play_container} onClick={playSong}>
+                    <div className={Styles.play_container} onClick={togglePlay}>
                         <img src='/images/play.png' className={Styles.play} alt='icon play de me-hira'/>
                     </div>
                     <div className={Styles.play_container}>
-                        <img src='/images/options.png' className={Styles.options} alt='icon opttions de me-hira'/>
+                        <img src='/images/options.png' className={Styles.options} alt='icon options de me-hira'/>
                     </div>
                 </section>
             </section>
         </li>
-    )
+    );
 }
 
-export default SongComponent
+export default SongComponent;
