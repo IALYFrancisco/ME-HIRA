0532397c804cd229f25d08355991f0604a3ea9ad
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-06-20 15:25:20 +0300

    dynamisation de la liste des chansons

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,41 +1,52 @@
-import ButtonComponent from '../components/ButtonComponent'
+import { useEffect, useState } from 'react';
+import ButtonComponent from '../components/ButtonComponent';
 import LogoComponent from '../components/LogoComponent';
 import SongComponent from '../components/Song/SongComponent';
 
-function HomeView () {
+function HomeView() {
+  const [songs, setSongs] = useState([]);
 
-    return (
-        <>
-            <header>
-                <nav>
-                    <div className="logo-container">
-                        <LogoComponent/>
-                </div>
-                    <div className="searchbar-container">
-                        <input type="text" name="" id="" placeholder="Faite une recherche ..."/>
-                    </div>
-                    <div className="button-container">
-                        <ButtonComponent text="Se connecter"/>
-                    </div>
-                </nav>
-            </header>
-            <main>
-                <aside></aside>
-                <section>
-                    <ul>
-                        <SongComponent author='Anatal' title='6000 euro' src='/images/Anatal - 6000 Euro ( Officiel Vidéo).mp3'/>
-                        <SongComponent author='Ceis' title='Mogou' src='/images/Ceis - Mougou (Official Music Video).mp3'/>
-                        <SongComponent author='Lion Hill' title='Angala fitia' src='/images/Lion Hill - Angala fitia (Official video).mp3'/>
-                        <SongComponent author='Rim Ka' title='Glita' src='/images/Rim-Ka - GLITA (Official Video Music).mp3'/>
-                        <SongComponent author='ZAKAI' title='MELODIE' src='/images/ZAKAI - MELODIE  (Clip officiel).mp3'/>
-                    </ul>
-                </section>
-                <aside></aside>
-            </main>
-        </>
-    )
+  useEffect(() => {
+    fetch('https://mehiraserver.onrender.com/song/get')
+      .then((res) => res.json())
+      .then((data) => setSongs(data))
+      .catch((err) => console.error("Erreur lors du fetch des chansons:", err));
+  }, []);
 
-    
+  return (
+    <>
+      <header>
+        <nav>
+          <div className="logo-container">
+            <LogoComponent />
+          </div>
+          <div className="searchbar-container">
+            <input type="text" placeholder="Faites une recherche ..." />
+          </div>
+          <div className="button-container">
+            <ButtonComponent text="Se connecter" />
+          </div>
+        </nav>
+      </header>
+
+      <main>
+        <aside></aside>
+        <section>
+          <ul>
+            {songs.map(song => (
+              <SongComponent
+                key={song._id}
+                title={song.title}
+                author={song.author}
+                src={song.file_url}
+              />
+            ))}
+          </ul>
+        </section>
+        <aside></aside>
+      </main>
+    </>
+  );
 }
 
 export default HomeView;
