58811ac0f7c0a3347ed261a49db6858752fd2a66
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-06-20 13:24:05 +0300

    définition du logique du context

---
+++ src/contexts/SongContext.js
@@ -0,0 +1,30 @@
+import { createContext, useRef, useState } from "react";
+
+export const AudioContext = createContext();
+
+export const AudioProvider = ({ children }) => {
+  const [currentAudio, setCurrentAudio] = useState(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const play = (audioElement) => {
+    if (currentAudio && currentAudio !== audioElement) {
+      currentAudio.pause();
+      currentAudio.currentTime = 0;
+    }
+
+    if (audioElement === currentAudio && isPlaying) {
+      audioElement.pause();
+      setIsPlaying(false);
+    } else {
+      audioElement.play();
+      setCurrentAudio(audioElement);
+      setIsPlaying(true);
+    }
+  };
+
+  return (
+    <AudioContext.Provider value={{ play }}>
+      {children}
+    </AudioContext.Provider>
+  );
+};
